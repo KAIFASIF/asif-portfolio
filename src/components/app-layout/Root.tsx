f871fe12cfd2a1e8b57bda18813fbb8024b8d34e
kaif <asifkaif21@gmail.com>
2023-08-10 15:29:40 +0530

    Sticky Navbar

--- src/components/app-layout/Root.tsx
+++ src/components/app-layout/Root.tsx
@@ -5,15 +5,18 @@ import Sidebar from "../Sidebar";
 const Root = () => {
   const location = useLocation().pathname === "/resume";
   return (
-    // <div className="flex  felx-col top-0 w-full">
-    <div className="flex  felx-col top-0 w-full overflow-x-hidden lg:overflow-x-clip">
+    <div className="flex  felx-col top-0 w-full">
+    {/* <div className="flex  felx-col top-0 w-full  overflow-x-hidden lg:overflow-x-clip"> */}
       {!location && (
         <aside className="bg-black sticky h-[100vh] top-0 w-14 hidden lg:block">
           <Sidebar />
         </aside>
       )}
       <main className="bg-black flex-1">
-        {!location && <Navbar />}
+      {!location && (
+        
+          <Navbar />
+      )}
         <Outlet />
       </main>
     </div>
